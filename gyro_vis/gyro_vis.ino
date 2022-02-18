825edf2f4bfdee0464bed8e2d264a160b099d159
Your Name <anto1013.ei@rmkec.ac.in>
2022-02-18 16:15:49 +0530

    gyro visualisation code

---
+++ gyro_vis/gyro_vis.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>;
+
+const int MPU_addr = 0x68;
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("Press any key");
+    while(Serial.available()==0);
+    Wire.begin(4,5,100000);
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x6B);
+    Wire.write(0x00);
+    Wire.endTransmission(true);
+    delay(50);
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x1B);
+    Wire.write(0x10);
+    Wire.endTransmission(true);
+    Wire.beginTransmission(MPU_addr); 
+    Wire.write(0x1C);
+    Wire.write(0x00); 
+    Wire.endTransmission(true);
+}
+
+void loop()
+{
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x41);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr, 2, true);
+    int16_t tempFull = Wire.read() << 8 | Wire.read();
+    float tempFinal = (float) tempFull / 340.0 + 36.53;
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x43);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr, 6, true);
+    int16_t XGyroFull = Wire.read() << 8 | Wire.read();
+    int16_t YGyroFull = Wire.read() << 8 | Wire.read();
+    int16_t ZGyroFull = Wire.read() << 8 | Wire.read();
+    float XGyroFinal = (float)XGyroFull/32.8;
+    float YGyroFinal = (float)YGyroFull/32.8;
+    float ZGyroFinal = (float)ZGyroFull/32.8;
+    Serial.print(XGyroFinal);
+    Serial.print(" ");
+    Serial.print(YGyroFinal);
+    Serial.print(" ");
+    Serial.print(ZGyroFinal);
+    delay(100);
+}
